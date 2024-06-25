561bca9a46a288478d152612c56ff3f4fe81b64b
mr-santos <mr-santos@mr-santos.mr-santos>
2024-06-25 16:02:05 -0300

    Add files and images in project

--- lib/src/images/babigol_images.dart
+++ lib/src/images/babigol_images.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 
 class BabigolImages{
   static const String _packageName = 'babigol';
-  static const repenseLogo = AssetImage(package: _packageName, 'assets/logo.png');
-  static const repenseLogoLapis = AssetImage(package: _packageName, 'assets/logo-astro-lapis.png');
-  static const repenseLogoNome = AssetImage(package: _packageName, 'assets/logo-nome.png');
-  static const repenseBalao = AssetImage(package: _packageName, 'assets/logo-astro-balao.png');
+  static const logo = AssetImage(package: _packageName, 'assets/images/logo.png');
+  static const logoNome = AssetImage(package: _packageName, 'assets/images/logo_nome.png');
+  //static const repenseLogoLapis = AssetImage(package: _packageName, 'assets/logo-astro-lapis.png');
+  //static const repenseBalao = AssetImage(package: _packageName, 'assets/logo-astro-balao.png');
 }
