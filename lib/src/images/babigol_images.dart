727c73ff88d4c7240e961af4c27fc1b1f262615b
mr-santos <mr-santos@mr-santos.mr-santos>
2024-06-26 16:02:57 -0300

    Add images in project

--- lib/src/images/babigol_images.dart
+++ lib/src/images/babigol_images.dart
@@ -3,7 +3,8 @@ import 'package:flutter/material.dart';
 class BabigolImages{
   static const String _packageName = 'babigol';
   static const logo = AssetImage(package: _packageName, 'assets/images/logo.png');
-  static const logoNome = AssetImage(package: _packageName, 'assets/images/logo_nome.png');
+  static const logoNome = AssetImage(package: _packageName, 'assets/images/logo_name.png');
   //static const repenseLogoLapis = AssetImage(package: _packageName, 'assets/logo-astro-lapis.png');
   //static const repenseBalao = AssetImage(package: _packageName, 'assets/logo-astro-balao.png');
+  ///home/mr-santos/Área de trabalho/cods/reidabola/babigol/assets/images/logo.png
 }
