440d0e51f98a701f1916a6e244d4b137f456d910
mr-santos <mr-santos@mr-santos.mr-santos>
2024-06-28 14:47:22 -0300

    Create in new widgets in desing system

--- lib/babigol.dart
+++ lib/babigol.dart
@@ -6,3 +6,4 @@ export 'src/form_field/babigol_form_field.dart';
 export 'src/masked/maskeds.dart';
 export 'src/images/babigol_images.dart';
 export 'src/buttons/buttons.dart';
+export 'src/widgets/widgets.dart';
